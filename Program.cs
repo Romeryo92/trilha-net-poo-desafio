b72138225a5b101962df3d80ffcf096a64c629e4
Romeryo92 <romeryo_lenilson@hotmail.com>
2024-08-26 09:39:53 -0300

    Desafio Finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Teste com Nokia
+        Smartphone nokia = new Nokia("122156659", "Nokia Modelo", "121212121212", 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        // Teste com Iphone
+        Smartphone iphone = new Iphone("99885540", "iPhone Modelo", "202020202020", 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
